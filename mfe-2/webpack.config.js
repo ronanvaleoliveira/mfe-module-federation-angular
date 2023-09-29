28f1e343c07028ff3e4e6850d60356d71e2e14bb
Ronan Vale de Oliveira <ronan.oliveira@totvs.com.br>
2023-09-29 18:05:07 -0300

    Projeto mfe-2

---
+++ mfe-2/webpack.config.js
@@ -0,0 +1,23 @@
+const {
+  shareAll,
+  withModuleFederationPlugin
+} = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  name: 'mfe-2',
+
+  exposes: {
+    //'./Component': './src/app/app.component.ts',
+    './Module': './src/app/app.module.ts',
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto'
+    }),
+  },
+
+});
